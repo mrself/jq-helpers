fdc1a1384a058272083394e5b935c026cfab7311
Lightman <yaruk.my@gmail.com>
2016-04-12 05:27:40 +0300

    feat: add #debounce

--- src.js
+++ src.js
@@ -34,6 +34,30 @@
 			$('html, body').animate({
 	            scrollTop: $(this).offset().top
 	        }, duration);
+		},
+		debounce: function(namespace, eventName, interval){
+		    // deBouncer by hnldesign.nl
+		    // more info: http://www.hnldesign.nl/work/code/debouncing-events-with-jquery/
+		    //
+		    // based on code by Paul Irish and the original debouncing function from John Hann
+		    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+		    var debounce = function (func, threshold, execAsap) {
+		        var timeout;
+		        return function debounced () {
+		            var obj = this, args = arguments;
+		            function delayed () {
+		                if (!execAsap)
+		                    func.apply(obj, args);
+		                timeout = null;
+		            }
+		            if (timeout)
+		                clearTimeout(timeout);
+		            else if (execAsap)
+		                func.apply(obj, args);
+		            timeout = setTimeout(delayed, threshold || interval);
+		        };
+		    };
+		    jQuery.fn[eventName + '.' + namespace] = function(fn){  return fn ? this.bind(of, debounce(fn)) : this.trigger(cf); };
 		}
 	});
 	
